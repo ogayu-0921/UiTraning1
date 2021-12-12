8fa53994d89d950467767a5c1aaa7ad601ba59ec
ogayu-0921 <ogayu0921@gmail.com>
2021-12-12 16:18:40 +0900

    実行すると何故か真っ白です。

--- app/src/main/java/com/example/jetpackcompose/uitraning1/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/uitraning1/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -34,7 +36,16 @@ private fun FirstView() {
             color = Color.White,
             modifier = Modifier
                 .padding(start = 40.dp, end = 40.dp, top = 8.dp, bottom = 8.dp)
-                .background(Color())
+                .background(Color(0xffa500))
+                .padding(start = 96.dp, end = 96.dp, top = 16.dp, bottom = 16.dp)
+        )
+        Text(
+            text = "運営から",
+            fontSize = 20.sp,
+            color = Color(0xffa500),
+            modifier = Modifier
+                .padding(start = 40.dp, end = 40.dp, top = 8.dp, bottom = 8.dp)
+                .background(Color.White)
                 .padding(start = 96.dp, end = 96.dp, top = 16.dp, bottom = 16.dp)
         )
     }
